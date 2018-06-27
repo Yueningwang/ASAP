5c50b7b4b0cf919fb9498f01e98abb2b7988a2d7
Cheng Zeng <zengc7@uw.edu>
2018-06-27 10:46:14 -0700

    Update Function

--- ASAP/Solar_Calculation.py
+++ ASAP/Solar_Calculation.py
@@ -11,31 +11,35 @@ def arc_beg(work_path):
     arcpy.env.overwriteOutput = True
     arcpy.CheckOutExtension("3D")  # load 3D Analyst tool
     arcpy.CheckOutExtension("Spatial")  # load Spatial Analyst tool
+    print("The workspace has set to:"+ work_path)
     return
 
 
-def las_to_raster(las, dem):
+def las_to_raster(las):
     """This function converts las data to las dataset, and turn the dataset into raster.
-    The input should be .las files or folder and the name of output raster file"""
+    The input should be .las files or folder and the ouput will be raster file"""
+    dem = 'DEM_raster'
     arcpy.CreateLasDataset_management(las, 'dataset.lasd', 'RECURSION')  # convert las data into las dataset
     arcpy.LasDatasetToRaster_conversion('dataset.lasd', dem, "ELEVATION", "BINNING AVERAGE NATURAL_NEIGHBOR",
                                         "FLOAT", "CELLSIZE", 1, 1)  # convert las dataset into raster
-    return
+    return dem
 
 
-def project_raster(dem, raster):
-    """This function is used to convert unit of coordination system from feet to meter.
-    The input should be a raster and the name of output raster file"""
+def project_raster(dem):
+    """This function is used to convert unit of corrdination system from feet to meter.
+    The input should be a raster and the output will be projected raster file"""
+    raster = 'projected_DEM'
     out_coordinate_system = arcpy.SpatialReference(26934)
-    arcpy.ProjectRaster_management(dem, 'XY_dem', out_coordinate_system, "BILINEAR", 1) #Project XY coordination system
+    arcpy.ProjectRaster_management(dem, 'XY_dem', out_coordinate_system, "BILINEAR") #Project XY coordination system
     pro_raster = Times('XY_dem', 0.3048) #Covert unit of z value from feet to meter
     pro_raster.save(raster)
-    return
+    return raster
 
-# need to test
-def create_mask(raster, mask):
+
+def create_mask(raster):
     """This function creates a mask to filter unsuitable location to install solar panels.
-    The input should be a raster and name of output mask raster"""
+    The input should be a raster and the output will be mask raster"""
+    mask = 'mask_raster'
     arcpy.Aspect_3d(raster, 'aspect')  # filter aspect
     # filter south facing or horizontal aspect. Flat, 112.5 <= aspect <= 247.5, set value to 1, others to None
     filter_aspect = Con((Raster('aspect') == -1) | (Raster('aspect') >= 112.5) & (Raster('aspect') <= 247.5), 1, '')
@@ -46,19 +50,20 @@ def create_mask(raster, mask):
     filter_slope.save("filtered_slope.tif")
     result = Times("filtered_aspect.tif", "filtered_slope.tif")  # Combine slope and aspect
     result.save(mask)
-    return
+    return mask
 
 
-def solar_radiation(mask, raster, solar, polygon):
+def solar_radiation(mask, raster, polygon):
     """This function is used to calculate solar radiation of filtered location.
-    The input should be mask raster, DEM raster, polygon shape file and the name of output solar raster"""
+    The input should be mask raster, DEM raster, polygon shape file and the output will be solar raster"""
+    solar = 'solar_raster'
     result = Times(mask, raster)
     result.save("result")
     polygon_raster = ExtractByMask("result", polygon)  # extract by polygon of buildings or parking lots
     polygon_raster.save("poly_raster")
-    solar_rad = AreaSolarRadiation("poly_raster", time_configuration=TimeWholeYear(2018),
-                                   out_direct_duration_raster='solar_dur')  # calculate solar radiation
-    solar_rad.save("solar_rad")  # unit WH/m2
+    solar_radiation = AreaSolarRadiation("poly_raster", time_configuration=TimeWholeYear(2018), 
+                                         out_direct_duration_raster='solar_dur')  # calculate solar radiation
+    solar_radiation.save("solar_rad")  # unit WH/m2
     solar_result = Divide('solar_rad', "solar_dur")
     solar_result.save(solar)  # unit W/m2
-    return
+    return solar
