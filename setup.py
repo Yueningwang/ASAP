98273df19e95951966cd0c595f918d5eafc11a6c
Cheng Zeng <zengc7@uw.edu>
2018-06-28 13:04:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='Solar_Calculation',
-    version='0.1dev0',
-    packages=find_packages(exclude=['tests']),
-    license='MIT',
+    version='0.1dev1',
+    description='Identify potential solar sites and compute potential solar energy',
     long_description=open('README.md').read(),
     url='https://github.com/Yueningwang/ASAP',
+    author='Jingtian Zhang, Cheng Zeng, Yuening Wang',
+    author_email='zengcheng95@gmail.com',
+    license='MIT',
+    keywords='solar energy',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'python_version' == '2.7',
+        'arcpy' >= 1.0
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/Yueningwang/ASAP/issues',
+        'Source': 'https://github.com/Yueningwang/ASAP',
+    },
 )
