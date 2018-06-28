c3bd156f571a78e82464b7013c61389de385cb09
Cheng Zeng <zengc7@uw.edu>
2018-06-28 10:47:30 -0700

    Finsih module

--- ASAP/Solar_Calculation.py
+++ ASAP/Solar_Calculation.py
@@ -71,3 +71,75 @@ def solar_radiation(mask, raster, polygon):
     solar_result.save(solar)  # unit W/m2
     ZonalStatisticsAsTable(polygon, 'FID', solar, solar_tab, "DATA", "SUM")  # generate solar energy table
     return solar_tab
+
+
+def join_shape(solar_tab, building, address):
+    """This function is used to join the solar energy table with building and address shape files
+    The input should be table, building and address shape files, and the output should be joined shape file"""
+    sol_build = 'solar_build'
+    build_addr = 'addr_build'
+    arcpy.AddIndex_management(solar_tab, 'FID')  # Index.
+    # join table to building
+    arcpy.MakeFeatureLayer_management(building, 'build_layer')
+    arcpy.AddJoin_management('build_layer', 'FID', solar_tab, 'FID', 'KEEP_COMMON')
+    arcpy.CopyFeatures_management('build_layer', 'solar_build')
+    # join address to building
+    arcpy.SpatialJoin_analysis('solar_build', address, build_addr,
+                           'JOIN_ONE_TO_ONE', 'KEEP_COMMON', field_mappings(sol_build, address), 'CLOSEST')
+    return build_addr
+
+
+def field_mappings(sol_build, address):
+    """This function creates field mappings for other function"""
+    fms = arcpy.FieldMappings()
+    # create field maps
+    f_st_add = arcpy.FieldMap()
+    f_st_add.addInputField(address, 'ST_ADD')
+    f_cityzip = arcpy.FieldMap()
+    f_cityzip.addInputField(address,'CITYSTZIP')
+    f_council = arcpy.FieldMap()
+    f_council.addInputField(address, 'COUNCIL')
+    f_area = arcpy.FieldMap()
+    f_area.addInputField(sol_build, 'solar_en_3')
+    f_solar = arcpy.FieldMap()
+    f_solar.addInputField(sol_build, 'solar_en_4')
+    # rename field maps
+    area_name = f_area.outputField
+    area_name.name = 'AREA'
+    f_area.outputField = area_name
+    solar_name = f_solar.outputField
+    solar_name.name = 'SOLAR'
+    f_solar.outputField = solar_name
+    # add field maps to field mappings
+    fms.addFieldMap(f_st_add)
+    fms.addFieldMap(f_cityzip)
+    fms.addFieldMap(f_council)
+    fms.addFieldMap(f_area)
+    fms.addFieldMap(f_solar)
+    return fms
+
+
+def generate_result(shape):
+    """This function converts shape file into excel and kml files.
+    The input should be shape file, and the output will be excel and kml files"""
+    table = 'fin_result.xls'
+    kml = 'fin_result.kml'
+    arcpy.TableToExcel_conversion(shape, table)
+    arcpy.MakeFeatureLayer_management(shape, 'layer')
+    arcpy.LayerToKML_conversion('layer', kml)
+    return table, kml
+
+
+def main(work_path, las, building, address):
+    """This is the main function of the module.
+    The input should be the path all files store, .las files or folder, building shapefile, and address shape file.
+    The output will be .xls file and .kml file""""
+    arc_beg(work_path)
+    dem = las_to_raster(las)
+    raster = project_raster(dem)
+    mask = create_mask(raster)
+    solar_tab = solar_radiation(mask, raster, building)
+    build_addr = join_shape(solar_tab, building, address)
+    generate_result(build_addr)
+    print("Work completed. The output files fin_result.xls and fin_result.kml are saved to " + work_path)
+    return
